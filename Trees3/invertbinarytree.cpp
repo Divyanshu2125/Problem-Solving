f0c452a95378ce442a29de3bd9ff96c67ba8bd41
Divyanshu Kaushal <123375624+Divyanshu2125@users.noreply.github.com>
2024-03-29 00:09:14 +0530

    Create invertbinarytree.cpp

---
+++ Trees3/invertbinarytree.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+         if(root==NULL) return NULL;
+       invertTree(root->left);
+       invertTree(root->right);
+       TreeNode* le=root->left;
+       root->left=root->right;
+       root->right=le;
+      
+       return root;
+    }
+};
