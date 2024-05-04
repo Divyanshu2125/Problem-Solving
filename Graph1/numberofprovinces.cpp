13c6de7756ed8388597e055160b4f9e98c4575dd
Divyanshu Kaushal <123375624+Divyanshu2125@users.noreply.github.com>
2024-05-05 00:37:10 +0530

    Update numberofprovinces.cpp

--- Graph1/numberofprovinces.cpp
+++ Graph1/numberofprovinces.cpp
@@ -1,37 +1,68 @@
- void dfs(int curr,vector<int> adj[],vector<int>& visited)
- {
-    visited[curr]=1;
-    for(auto i:adj[curr])
-    {
-        if(visited[i]==0)
-        {
-            dfs(i,adj,visited);
-        }
-    }
- }
-    int findCircleNum(vector<vector<int>>& isConnected) {
-     int V=isConnected.size();
-     vector<int> adj[V];
-     for(int i=0;i<V;i++)
-     {
-        for(int j=0;j<V;j++)
-        {
-            if(isConnected[i][j]==1 && i!=j)
-            {
-                adj[i].push_back(j);
-                adj[j].push_back(i);
+class Solution {
+public:
+    
+    void dfs(int src,vector<vector<int>>& isConnected,unordered_map<int,bool> &visited){
+        visited[src]=true;
+        int col=isConnected.size();
+        int row=src;
+        for(int i=0;i<col;i++){
+            if(isConnected[row][i]==1){
+                if(!visited[i]){
+                    dfs(i,isConnected,visited);
+                }
             }
         }
-     }  
-     vector<int> visited(V,0);
-     int count=0;
-     for(int i=0;i<V;i++)
-    {
-        if(visited[i]==0)
-        {
-            count++;
-            dfs(i,adj,visited);
-        }
-    } 
-    return count;
     }
+    
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n=isConnected.size();
+        unordered_map<int,bool> visited;
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(!visited[i]){
+                dfs(i,isConnected,visited);
+                count++;
+            }
+        }
+        return count;
+    }
+};
+
+// void dfs(int curr,vector<int> adj[],vector<int>& visited)
+ // {
+ //    visited[curr]=1;
+ //    for(auto i:adj[curr])
+ //    {
+ //        if(visited[i]==0)
+ //        {
+ //            dfs(i,adj,visited);
+ //        }
+ //    }
+ // }
+ //    int findCircleNum(vector<vector<int>>& isConnected) {
+ //     int V=isConnected.size();
+ //     vector<int> adj[V];
+ //     for(int i=0;i<V;i++)
+ //     {
+ //        for(int j=0;j<V;j++)
+ //        {
+ //            if(isConnected[i][j]==1 && i!=j)
+ //            {
+ //                adj[i].push_back(j);
+ //                adj[j].push_back(i);
+ //            }
+ //        }
+ //     }  
+ //     vector<int> visited(V,0);
+ //     int count=0;
+ //     for(int i=0;i<V;i++)
+ //    {
+ //        if(visited[i]==0)
+ //        {
+ //            count++;
+ //            dfs(i,adj,visited);
+ //        }
+ //    } 
+ //    return count;
+ //    }
+
