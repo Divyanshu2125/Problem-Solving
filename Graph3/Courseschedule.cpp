73bc4d2e2b92b4792b6e7bfc1a6a8d1a2b232557
Divyanshu Kaushal <123375624+Divyanshu2125@users.noreply.github.com>
2024-05-08 01:16:45 +0530

    Create Courseschedule.cpp

---
+++ Graph3/Courseschedule.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    
+    void topoSort(int n,vector<int> &topo,unordered_map<int,list<int>> &adjList){
+        queue<int> q;
+        map<int,int> indegree;
+        for(auto i:adjList){
+            for(auto nbr : i.second){
+                indegree[nbr]++;
+            }
+        }
+        for(int node=0;node<n;node++){
+            if(indegree[node]==0){
+                q.push(node);
+            }
+        }
+        while(!q.empty()){
+            int fnode=q.front();
+            q.pop();
+            topo.push_back(fnode);
+            for(auto nbr : adjList[fnode]){
+                indegree[nbr]--;
+                if(indegree[nbr]==0){
+                    q.push(nbr);
+                }
+            }
+        }
+    }
+    
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        unordered_map<int,list<int>> adjList;
+        for(auto i:prerequisites){
+            int u=i[0];
+            int v=i[1];
+            adjList[u].push_back(v);
+        }
+        vector<int> topo;
+        topoSort(numCourses,topo,adjList);
+        if(topo.size()==numCourses){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
+};
